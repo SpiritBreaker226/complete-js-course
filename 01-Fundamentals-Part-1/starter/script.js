45df04b93457f94fa954e7bd305829749b8d6e3a
Jason Stathopulos <jstathopulos@gmail.com>
2022-11-15 11:24:06 -0800

    Show How One Change Can Affect Display
    Show how one change to a variable can affect the display of all
    variables with that same name.

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -4,7 +4,8 @@ console.log(40+8+23-10)
 console.log('Jones')
 console.log(23)
 
-let firstName = 'Jones'
+let firstName = 'Matilda'
 console.log(firstName)
-
+console.log(firstName)
+console.log(firstName)
 
