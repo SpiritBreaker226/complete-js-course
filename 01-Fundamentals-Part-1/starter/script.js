7c3b8cdaebb98f15c86691f0a6d1a9106e8d2e96
Jason Stathopulos <jstathopulos@gmail.com>
2022-11-15 18:38:36 -0800

    Add Strings and Template Literals Lesson

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -173,3 +173,31 @@ markHigherBMI = markBmi > johnBmi
 console.log("Mark's BMI higher then John's", markHigherBMI)
 
 console.log("=============================")
+
+// Strings and Template Literals
+
+const firstName = 'Jonas';
+const job = 'teacher';
+const birthYear = 1991;
+const year = 2037;
+
+// the old way
+
+const jonas = "I'm " + firstName + ', a ' + (year - birthYear) + ' year old ' + job + '!';
+console.log(jonas);
+
+// the new and perfer way
+
+const jonasNew = `I'm ${firstName}, a ${year - birthYear} year old ${job}!`;
+console.log(jonasNew);
+
+console.log(`Just a regular string...`);
+
+console.log('String with \n\
+multiple \n\
+lines');
+
+console.log(`String
+multiple
+lines`);
+
