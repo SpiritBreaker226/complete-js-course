cc2a97fbe1ffe3886a43521c63683fd695bafd81
Jason Stathopulos <jstathopulos@gmail.com>
2022-11-15 17:38:54 -0800

    Add Data Types Lesson

--- 01-Fundamentals-Part-1/starter/script.js
+++ 01-Fundamentals-Part-1/starter/script.js
@@ -35,3 +35,25 @@ let job1 = 'Programmer'
 let job2 = 'Teacher'
 
 */
+
+// Data Types
+
+let javascriptIsFun = true;
+console.log(javascriptIsFun);
+
+// console.log(typeof true);
+console.log(typeof javascriptIsFun);
+// console.log(typeof 23);
+// console.log(typeof 'Jonas');
+
+javascriptIsFun = 'YES!';
+console.log(typeof javascriptIsFun);
+
+let year;
+console.log(year);
+console.log(typeof year);
+
+year = 1991;
+console.log(typeof year);
+
+console.log(typeof null);
