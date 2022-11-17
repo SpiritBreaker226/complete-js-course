3036760a778550fc2a7169510fe8fd8ca9789cbe
Jason Stathopulos <jstathopulos@gmail.com>
2022-11-17 10:04:02 -0800

    Add Prettier & Example

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,10 @@
 // Remember, we're gonna use strict mode in all scripts now!
 'use strict';
 
+const xy = 23;
+
+if (xy === 23) {
+  console.log(23);
+}
+
+const calcAge = birthYear => 2037 - birthYear;
