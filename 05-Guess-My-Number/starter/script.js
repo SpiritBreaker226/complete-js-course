8436974641c6daace0f24e9f79c67292b94124b4
Jason Stathopulos <jstathopulos@gmail.com>
2022-11-18 15:50:30 -0800

    Remove Console Log From Guess Number

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -19,8 +19,6 @@ let hightscore = 0;
 document.querySelector('.check').addEventListener('click', e => {
   const guess = Number(document.querySelector('.guess').value);
 
-  console.log(guess, typeof guess);
-
   if (!guess) {
     document.querySelector('.message').textContent = '⛔️ No number!';
 
