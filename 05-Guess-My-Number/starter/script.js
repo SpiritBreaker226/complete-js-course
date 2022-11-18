a14bf32842ffb4026b24df4d60b0fca4881f4c54
Jason Stathopulos <jstathopulos@gmail.com>
2022-11-18 15:20:26 -0800

    Move Display Secret Number to Winning Game
    Move the Secret Number to be displayed when you win the game in Guess
    Number so that the user can see the correct number when they win the
    game.

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -15,8 +15,6 @@ document.querySelector('.guess').value = 23;
 const secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
 
-document.querySelector('.number').textContent = secretNumber;
-
 document.querySelector('.check').addEventListener('click', e => {
   const guess = Number(document.querySelector('.guess').value);
 
@@ -31,6 +29,8 @@ document.querySelector('.check').addEventListener('click', e => {
   // When they win
   if (guess === secretNumber) {
     document.querySelector('.message').textContent = '🎉 Correct Number!';
+
+    document.querySelector('.number').textContent = secretNumber;
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
