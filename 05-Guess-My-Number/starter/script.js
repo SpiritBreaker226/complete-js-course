6e3ea23544400069a2aab0767284dd259c454e9c
Jason Stathopulos <jstathopulos@gmail.com>
2022-11-18 15:48:38 -0800

    Add Highscore Check For Guess Number

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -14,6 +14,7 @@ document.querySelector('.guess').value = 23;
 
 let secretNumber = Math.trunc(Math.random() * 20) + 1;
 let score = 20;
+let hightscore = 0;
 
 document.querySelector('.check').addEventListener('click', e => {
   const guess = Number(document.querySelector('.guess').value);
@@ -34,6 +35,11 @@ document.querySelector('.check').addEventListener('click', e => {
 
     document.querySelector('body').style.backgroundColor = '#60b347';
     document.querySelector('.number').style.width = '30rem';
+
+    if (score > hightscore) {
+      hightscore = score;
+      document.querySelector('.highscore').textContent = score;
+    }
 
     return;
   }
