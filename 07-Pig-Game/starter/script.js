55150e68dc00c83a0f54a3ddaadf4aad96abb56d
Jason Stathopulos <jstathopulos@gmail.com>
2022-11-18 18:06:05 -0800

    Add Most of Rolling Dice Functionality in Pig Game
    Add most of the rolling dice functionality in Pig Game so the user can
    roll the dice. The one thing that needs to be added is that the player
    does not switch on dice roll results 1, as to the game's rules.

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -1,11 +1,43 @@
 'use strict';
 
-// Selecting elements
+// Players
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
+const current0El = document.querySelector('#current--0');
+const current1El = document.querySelector('#current--1');
+
+// Dice
 const diceEl = document.querySelector('.dice');
+
+// Buttons
+const btnRollDice = document.querySelector('.btn--roll');
+
+let currentScore = 0;
 
 score0El.textContent = 0;
 score1El.textContent = 0;
 
 diceEl.classList.add('hidden');
+
+// Rolling dice functionality
+btnRollDice.addEventListener('click', () => {
+  // 1. Generating a random dice roll
+  const dice = Math.trunc(Math.random() * 6) + 1;
+
+  // 2. Display dice
+  diceEl.src = `dice-${dice}.png`;
+  diceEl.classList.remove('hidden');
+
+  // 3. Check for rolled 1: fi true
+  if (dice !== 1) {
+    // Add dice to current score
+    currentScore += dice;
+
+    // TODO CHange later
+    current0El.textContent = currentScore;
+
+    return;
+  }
+
+  // switch to next player
+});
