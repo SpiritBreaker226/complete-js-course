ef3b9f21ed32640d8d49f1cd9a1c6702db79829c
Jason Stathopulos <jstathopulos@gmail.com>
2022-11-18 18:43:01 -0800

    Add Hold Click For Pig Game
    Add the hold click for Pig Game so the players can keep their score
    while playing the game.

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -13,7 +13,9 @@ const diceEl = document.querySelector('.dice');
 
 // Buttons
 const btnRollDice = document.querySelector('.btn--roll');
+const btnHold = document.querySelector('.btn--hold');
 
+const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
@@ -21,6 +23,16 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 
 diceEl.classList.add('hidden');
+
+const switchPlayer = () => {
+  document.querySelector(`#current--${activePlayer}`).textContent = 0;
+  currentScore = 0;
+
+  activePlayer = activePlayer === 0 ? 1 : 0;
+
+  player0El.classList.toggle('player--active');
+  player1El.classList.toggle('player--active');
+};
 
 // Rolling dice functionality
 btnRollDice.addEventListener('click', () => {
@@ -43,13 +55,28 @@ btnRollDice.addEventListener('click', () => {
   }
 
   // switch to next player
+  switchPlayer();
+});
 
-  // udpate scurrent score
-  document.querySelector(`#current--${activePlayer}`).textContent = 0;
-  currentScore = 0;
+btnHold.addEventListener('click', () => {
+  // 1. Add current score to active player's score
+  scores[activePlayer] += currentScore;
+  document.querySelector(`#score--${activePlayer}`).textContent =
+    scores[activePlayer];
 
-  activePlayer = activePlayer === 0 ? 1 : 0;
+  // 2. CHeck if player is 100
+  if (scores[activePlayer] >= 100) {
+    // end gam
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.add('player--winner');
+    document
+      .querySelector(`.player--${activePlayer}`)
+      .classList.remove('player--active');
 
-  player0El.classList.toggle('player--active');
-  player1El.classList.toggle('player--active');
+    return;
+  }
+
+  // switch players
+  switchPlayer();
 });
