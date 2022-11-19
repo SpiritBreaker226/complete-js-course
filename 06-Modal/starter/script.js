bfecb1b05b809f5dfc979a29695dd0175377be1c
Jason Stathopulos <jstathopulos@gmail.com>
2022-11-18 16:34:59 -0800

    Add Variables to Hold HTML Elements
    Add the variables to hold HTML elements for Modal Example so that the
    developer can change the HTML elements without using the extended
    version to get the HTML element and make it easier to read.

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,8 @@
 'use strict';
+
+const modal = document.querySelector('.modal');
+const overlay = document.querySelector('.overlay');
+const btnCloseModal = document.querySelector('.close-modal');
+const btnShowModals = document.querySelectorAll('.show-modal');
+
+btnShowModals.forEach(btnShowModal => console.log(btnShowModal.textContent));
