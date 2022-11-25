4366e6655872e7e77e2a763d46556714568baff6
Jason Stathopulos <jstathopulos@gmail.com>
2022-11-24 20:27:11 -0800

    Add Coding Challenge 2 For Functions

--- 10-Functions/starter/script.js
+++ 10-Functions/starter/script.js
@@ -194,3 +194,40 @@ document
   .addEventListener('click', poll.registerNewAnswer.bind(poll));
 
 console.log('=============================');
+
+// Coding Challenge #2
+
+/*
+This is more of a thinking challenge than a coding challenge 🤓
+
+Take the IIFE below and at the end of the function, attach an event listener that changes the color of the selected h1 element ('header') to blue, each time the BODY element is clicked. Do NOT select the h1 element again!
+
+And now explain to YOURSELF (or someone around you) WHY this worked! Take all the time you need. Think about WHEN exactly the callback function is executed, and what that means for the variables involved in this example.
+
+GOOD LUCK 😀
+*/
+
+console.log('Coding Challenge #2');
+
+(function () {
+  const header = document.querySelector('h1');
+  header.style.color = 'red';
+
+  document.querySelector('body').addEventListener('click', function () {
+    header.style.color = 'blue';
+  });
+})();
+
+/*
+  When the page first loads up, the header is changed to red then, when it is
+  clicking, the header is changed to blue since the page does not reload when
+  and does not change back to red or the click event does not have that function.
+  It stays blue. The click event still happens, just that it continuously
+  changes to blue.
+
+  In terms of closure, since the parent function has the variable in its
+  Variable Environment the click event also has access to the style colour
+  so it too can change the colour.
+*/
+
+console.log('=============================');
