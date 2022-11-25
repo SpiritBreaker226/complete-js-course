a8613c4db85f222671bd4ef983f0d19b5c6d2ae8
Jason Stathopulos <jstathopulos@gmail.com>
2022-11-25 11:24:31 -0800

    Add Example For Array `forEach` & `Map`

--- 11-Arrays-Bankist/starter/script.js
+++ 11-Arrays-Bankist/starter/script.js
@@ -80,6 +80,18 @@ const displayMovements = movements => {
 
 displayMovements(account1.movements);
 
+const createUsernames = accounts => {
+  accounts.forEach(account => {
+    account.username = account.owner
+      .toLowerCase()
+      .split(' ')
+      .map(word => word[0])
+      .join('');
+  });
+};
+
+createUsernames(accounts);
+
 /////////////////////////////////////////////////
 /////////////////////////////////////////////////
 // LECTURES
