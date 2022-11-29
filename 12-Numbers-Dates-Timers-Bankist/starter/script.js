eda77aeace62eaf21ad9ef1f02a5db5072d20bdf
Jason Stathopulos <jstathopulos@gmail.com>
2022-11-28 17:47:00 -0800

    Add Date Time Operations Lession For Numbers

--- 12-Numbers-Dates-Timers-Bankist/starter/script.js
+++ 12-Numbers-Dates-Timers-Bankist/starter/script.js
@@ -16,14 +16,14 @@ const account1 = {
   pin: 1111,
 
   movementsDates: [
-    '2019-11-18T21:31:17.178Z',
-    '2019-12-23T07:42:02.383Z',
-    '2020-01-28T09:15:04.904Z',
-    '2020-04-01T10:17:24.185Z',
-    '2020-05-08T14:11:59.604Z',
-    '2020-05-27T17:01:17.194Z',
-    '2020-07-11T23:36:17.929Z',
-    '2020-07-12T10:51:36.790Z',
+    '2022-09-29T21:31:17.178Z',
+    '2022-10-11T07:42:02.383Z',
+    '2022-10-15T09:15:04.904Z',
+    '2022-11-01T10:17:24.185Z',
+    '2022-11-21T14:11:59.604Z',
+    '2022-11-22T17:01:17.194Z',
+    '2022-11-27T23:36:17.929Z',
+    '2022-11-28T23:59:17.929Z',
   ],
   currency: 'EUR',
   locale: 'pt-PT', // de-DE
@@ -36,14 +36,14 @@ const account2 = {
   pin: 2222,
 
   movementsDates: [
-    '2019-11-01T13:15:33.035Z',
-    '2019-11-30T09:48:16.867Z',
-    '2019-12-25T06:04:23.907Z',
-    '2020-01-25T14:18:46.235Z',
-    '2020-02-05T16:33:06.386Z',
-    '2020-04-10T14:43:26.374Z',
-    '2020-06-25T18:49:59.371Z',
-    '2020-07-26T12:01:20.894Z',
+    '2022-10-01T13:15:33.035Z',
+    '2022-10-30T09:48:16.867Z',
+    '2022-10-25T06:04:23.907Z',
+    '2022-10-31T14:18:46.235Z',
+    '2022-11-05T16:33:06.386Z',
+    '2022-11-10T14:43:26.374Z',
+    '2022-11-25T18:49:59.371Z',
+    '2022-11-26T12:01:20.894Z',
   ],
   currency: 'USD',
   locale: 'en-US',
@@ -81,6 +81,9 @@ const inputClosePin = document.querySelector('.form__input--pin');
 /////////////////////////////////////////////////
 // Functions
 
+const calcDaysPassed = (date1, date2) =>
+  Math.round(Math.abs(date2 - date1) / (1000 * 60 * 60 * 24));
+
 const getDateParts = date => {
   const day = `${date.getDate()}`.padStart(2, 0);
   const month = `${date.getMonth() + 1}`.padStart(2, 0);
@@ -95,6 +98,22 @@ const getDateParts = date => {
     hour,
     minutes,
   };
+};
+
+const formatMovements = date => {
+  const daysPassed = calcDaysPassed(new Date(), date);
+
+  if (daysPassed === 0) {
+    return 'Today';
+  } else if (daysPassed === 1) {
+    return 'Yesterday';
+  } else if (daysPassed <= 7) {
+    return `${daysPassed} days ago`;
+  }
+
+  const { day, month, year } = getDateParts(date);
+
+  return `${day}/${month}/${year}`;
 };
 
 const displayMovements = ({ movements, movementsDates }, isSort = false) => {
@@ -113,9 +132,7 @@ const displayMovements = ({ movements, movementsDates }, isSort = false) => {
     // please note that movementsDates[index] is not correct as the date does
     // not follow the transaction when sorted. this should be in an object with
     // the movement amount however, this is only practice.
-    const { day, month, year } = getDateParts(new Date(movementsDates[index]));
-
-    const displayDate = `${day}/${month}/${year}`;
+    const displayDate = formatMovements(new Date(movementsDates[index]));
 
     const html = `
       <div class="movements__row">
