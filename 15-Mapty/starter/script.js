f1d2db229a96e9a501b1a7af3310f46d15db23bf
Jason Stathopulos <jstathopulos@gmail.com>
2022-12-01 15:40:50 -0800

    Refactor Mapty to Use OOP Architecture
    Refactor Mapty to use the OOP architecture to show how OOP works in an
    actual application and clean up the code to be more readable.

--- 15-Mapty/starter/script.js
+++ 15-Mapty/starter/script.js
@@ -11,75 +11,91 @@ const inputDuration = document.querySelector('.form__input--duration');
 const inputCadence = document.querySelector('.form__input--cadence');
 const inputElevation = document.querySelector('.form__input--elevation');
 
-let map;
-let mapEvent;
+// Classes
 
-// to use the geo location from he browser
+class App {
+  #map;
+  #mapEvent;
 
-if (navigator.geolocation) {
-  navigator.geolocation.getCurrentPosition(
-    position => {
-      const { latitude, longitude } = position.coords;
-      const coords = [latitude, longitude];
+  constructor() {
+    this.#getPosition();
 
-      // map('map') is the id to the HTML tag in index.html
-      // L is the namespace for LeafletJS
-      // As LeafletJS is loading its code into the global object of the application
-      // so other JavaScript files can access it.
-      map = L.map('map').setView(coords, 13);
+    // handle events
 
-      // see for more details https://leafletjs.com/reference.html
-      L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
-        attribution:
-          '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
-      }).addTo(map);
+    form.addEventListener('submit', this.#newWorkout.bind(this));
+    inputType.addEventListener('change', this.#toggleElevationField);
+  }
 
-      // handling clicks on map
-      map.on('click', mapE => {
-        form.classList.remove('hidden');
+  #getPosition() {
+    if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(this.#loadMap.bind(this), () => {
+        // triggers when the user states they do not want the application to
+        // know their location
 
-        // sets the focus to the first text input
-        inputDistance.focus();
-
-        mapEvent = mapE;
+        alert('Could not get your position');
       });
-    },
-    () => {
-      // triggers when the user states they do not want the application to
-      // know their location
-
-      alert('Could not get your position');
     }
-  );
+  }
+
+  #loadMap(position) {
+    const { latitude, longitude } = position.coords;
+    const coords = [latitude, longitude];
+
+    // map('map') is the id to the HTML tag in index.html
+    // L is the namespace for LeafletJS
+    // As LeafletJS is loading its code into the global object of the application
+    // so other JavaScript files can access it.
+    this.#map = L.map('map').setView(coords, 13);
+
+    // see for more details https://leafletjs.com/reference.html
+    L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+      attribution:
+        '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors',
+    }).addTo(this.#map);
+
+    // handling clicks on map
+    this.#map.on('click', this.#showForm.bind(this));
+  }
+
+  #showForm(mapE) {
+    form.classList.remove('hidden');
+
+    // sets the focus to the first text input
+    inputDistance.focus();
+
+    this.#mapEvent = mapE;
+  }
+
+  #toggleElevationField() {
+    inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
+    inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
+  }
+
+  #newWorkout(e) {
+    e.preventDefault();
+
+    // clear inputs
+    inputDistance.value = '';
+    inputDuration.value = '';
+    inputCadence.value = '';
+    inputElevation.value = '';
+
+    // Display marker
+    const { lat, lng } = this.#mapEvent.latlng;
+    const popup = L.popup({
+      maxWidth: 250,
+      minWidth: 100,
+      autoClose: false,
+      closeOnClick: false,
+      className: 'running-popup',
+    });
+
+    L.marker([lat, lng])
+      .addTo(this.#map)
+      .bindPopup(popup)
+      .setPopupContent('Workout')
+      .openPopup();
+  }
 }
 
-form.addEventListener('submit', e => {
-  e.preventDefault();
-
-  // clear inputs
-  inputDistance.value = '';
-  inputDuration.value = '';
-  inputCadence.value = '';
-  inputElevation.value = '';
-
-  // Display marker
-  const { lat, lng } = mapEvent.latlng;
-  const popup = L.popup({
-    maxWidth: 250,
-    minWidth: 100,
-    autoClose: false,
-    closeOnClick: false,
-    className: 'running-popup',
-  });
-
-  L.marker([lat, lng])
-    .addTo(map)
-    .bindPopup(popup)
-    .setPopupContent('Workout')
-    .openPopup();
-});
-
-inputType.addEventListener('change', () => {
-  inputElevation.closest('.form__row').classList.toggle('form__row--hidden');
-  inputCadence.closest('.form__row').classList.toggle('form__row--hidden');
-});
+const app = new App();
