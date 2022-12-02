7b8d84783ec6bc61526e802405f8f4224932d8bb
Jason Stathopulos <jstathopulos@gmail.com>
2022-12-02 11:07:21 -0800

    Add Throwing Errors Manually Lesson For AJAX

--- 16-Asynchronous/starter/script.js
+++ 16-Asynchronous/starter/script.js
@@ -27,6 +27,14 @@ const renderError = function (msg) {
   countriesContainer.style.opacity = 1;
 };
 
+const getJSON = function (url, errorMsg = 'Something went wrong') {
+  return fetch(url).then(response => {
+    if (!response.ok) throw new Error(`${errorMsg} (${response.status})`);
+
+    return response.json();
+  });
+};
+
 ///////////////////////////////////////
 
 /*
@@ -172,3 +180,37 @@ const getCountryData = function (country) {
 getCountryData('usa');
 
 */
+
+// Throwing Errors Manually
+
+const getCountryData = function (country) {
+  // Country 1
+  getJSON(`https://restcountries.com/v2/name/${country}`, 'Country not found')
+    .then(data => {
+      renderCountry(data[0]);
+      const neighbour = data[0].borders[0];
+
+      if (!neighbour) throw new Error('No neighbour found!');
+
+      // Country 2
+      return getJSON(
+        `https://restcountries.com/v2/alpha/${neighbour}`,
+        'Country not found'
+      );
+    })
+
+    .then(data => renderCountry(data, 'neighbour'))
+    .catch(err => {
+      console.error(`${err} 💥💥💥`);
+      renderError(`Something went wrong 💥💥 ${err.message}. Try again!`);
+    })
+    .finally(() => {
+      countriesContainer.style.opacity = 1;
+    });
+};
+
+btn.addEventListener('click', function () {
+  getCountryData('canada');
+});
+
+getCountryData('australia');
