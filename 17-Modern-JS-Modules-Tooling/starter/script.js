29c5a1e5e7a133947d0e3ed2ac149fa8a88711ad
Jason Stathopulos <jstathopulos@gmail.com>
2022-12-05 15:49:18 -0800

    Add Using Parcel Lesson For Modules

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -139,3 +139,45 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 
 */
+
+// Using Parcel
+
+import cloneDeep from 'lodash-es';
+
+const state = {
+  cart: [
+    { product: 'bread', quantity: 5 },
+    { product: 'pizza', quantity: 5 },
+  ],
+  user: { loggedIn: true },
+};
+const stateClone = Object.assign({}, state);
+const stateDeepClone = cloneDeep(state);
+
+// this will auto-update the module and soft update
+// the page without having to do a full page refresh
+// this will not make it to production as it is strip out
+// because o because of the bundle handler
+if (module.hot) {
+  module.hot.accept();
+}
+
+state.user.loggedIn = false;
+console.log(stateClone);
+
+console.log(stateDeepClone);
+
+import add, { cart } from './shoppingCart.js';
+add('pizza', 2);
+add('bread', 5);
+add('apples', 4);
+
+console.log(cart);
+
+console.log(cart.find(el => el.quantity >= 2));
+Promise.resolve('TEST').then(x => console.log(x));
+
+import 'core-js/stable';
+
+// Polifilling async functions
+import 'regenerator-runtime/runtime';
