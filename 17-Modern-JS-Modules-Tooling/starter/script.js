fd86c1e1138febd181862af548e5785f8a03415d
Jason Stathopulos <jstathopulos@gmail.com>
2022-12-05 11:43:40 -0800

    Add CommonJS Modules Lesson For Modules

--- 17-Modern-JS-Modules-Tooling/starter/script.js
+++ 17-Modern-JS-Modules-Tooling/starter/script.js
@@ -81,3 +81,16 @@ console.log(ShoppingCart2);
 console.log(ShoppingCart2.shippingCost);
 
 */
+
+// CommonJS Modules
+
+// Export
+export.addTocart = function (product, quantity) {
+  cart.push({ product, quantity });
+  console.log(
+    `${quantity} ${product} added to cart (sipping cost is ${shippingCost})`
+  );
+};
+
+// Import
+const { addTocart } = require('./shoppingCart.js');
