5026b55495b529cd06a38d2b93092698d2be5dd0
Jason Stathopulos <jstathopulos@gmail.com>
2022-12-06 10:03:13 -0800

    Add Single Recipe Test API For Forkify

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -11,3 +11,37 @@ const timeout = function (s) {
 // https://forkify-api.herokuapp.com/v2
 
 ///////////////////////////////////////
+
+const showRecipe = async () => {
+  try {
+    const res = await fetch(
+      'https://forkify-api.herokuapp.com/api/v2/recipes/5ed6604591c37cdc054bc886'
+    );
+
+    const data = await res.json();
+
+    if (!res.ok) {
+      throw new Error(`${data.message} (${res.status})`);
+    }
+
+    const {
+      data: { recipe: apiRecipe },
+    } = data;
+    const recipe = {
+      id: apiRecipe.id,
+      title: apiRecipe.title,
+      publisher: apiRecipe.publisher,
+      sourceUrl: apiRecipe.source_url,
+      image: apiRecipe.image_url,
+      servings: apiRecipe.servings,
+      cookingTime: apiRecipe.cooking_time,
+      ingredients: apiRecipe.ingredients,
+    };
+
+    console.log('Recipe', recipe);
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+showRecipe();
