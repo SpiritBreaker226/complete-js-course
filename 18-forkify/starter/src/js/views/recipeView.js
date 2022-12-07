2dbcf715726a7b30eab8e3e1e65d38d5b06b7d5c
Jason Stathopulos <jstathopulos@gmail.com>
2022-12-06 17:18:54 -0800

    Add General Message For Recipe View
    Add a general message for Recipe View so we are ready when we search and
    allow us to have a standard message which we can display to the user.

--- 18-forkify/starter/src/js/views/recipeView.js
+++ 18-forkify/starter/src/js/views/recipeView.js
@@ -5,6 +5,7 @@ import icons from 'url:../../img/icons.svg';
 class RecipeView {
   #parentElement = document.querySelector('.recipe');
   #errorMessage = 'We could not find that recipe. Please try another one!';
+  #message = '';
   #data;
 
   render(data) {
@@ -35,6 +36,20 @@ class RecipeView {
       <div>
         <svg>
           <use href="${icons}#icon-alert-triangle"></use>
+        </svg>
+      </div>
+      <p>${message}</p>
+    </div>`;
+
+    this.#clear();
+    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
+  }
+
+  renderMessage(message = this.#message) {
+    const markup = `<div class="message">
+      <div>
+        <svg>
+          <use href="${icons}#icon-smile"></use>
         </svg>
       </div>
       <p>${message}</p>
