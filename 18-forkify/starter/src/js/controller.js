0df19483152aa050c96abe18dce3c432e8697768
Jason Stathopulos <jstathopulos@gmail.com>
2022-12-06 19:25:08 -0800

    Add Logic For Pagination on Search Results
    Add the logic for pagination on the search results, as some results are
    too large and may slow down the rendering. As a result, we need to break
    up the results and have the user look at one page at a time.

--- 18-forkify/starter/src/js/controller.js
+++ 18-forkify/starter/src/js/controller.js
@@ -40,7 +40,7 @@ const controlSearchResults = async () => {
 
     await model.loadSearchResults(query);
 
-    resultsView.render(model.state.search.results);
+    resultsView.render(model.getSearchResultsPage());
   } catch (error) {
     resultsView.renderError();
   }
--- 18-forkify/starter/src/js/model.js
+++ 18-forkify/starter/src/js/model.js
@@ -1,4 +1,4 @@
-import { API_URL } from './config';
+import { API_URL, RESULTS_PER_PAGE } from './config';
 import { getJSON } from './helpers';
 
 export const state = {
@@ -6,6 +6,8 @@ export const state = {
   search: {
     query: '',
     results: [],
+    resultsPerPage: RESULTS_PER_PAGE,
+    page: 1,
   },
 };
 
@@ -39,4 +41,13 @@ export const loadSearchResults = async query => {
     publisher: recipe.publisher,
     image: recipe.image_url,
   }));
+};
+
+export const getSearchResultsPage = (page = state.search.page) => {
+  state.search.page = page;
+
+  const start = (page - 1) * state.search.resultsPerPage;
+  const end = page * state.search.resultsPerPage;
+
+  return state.search.results.slice(start, end);
 };
