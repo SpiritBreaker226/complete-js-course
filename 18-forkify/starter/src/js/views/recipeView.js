36cee14f6978f8f6c15aee640a922efd119b0ebd
Jason Stathopulos <jstathopulos@gmail.com>
2022-12-06 18:21:36 -0800

    Add View Class For General View Functions
    Add a View class for general view functions since the number of views is
    growing, and they have the same functions for search and recipe details
    to keep the maintainable code moving the functions that all views will
    be needed to a generic class.

--- 18-forkify/starter/src/js/views/recipeView.js
+++ 18-forkify/starter/src/js/views/recipeView.js
@@ -1,63 +1,10 @@
 import { Fraction } from 'fractional';
 
 import icons from 'url:../../img/icons.svg';
+import View from './view';
 
-class RecipeView {
-  #parentElement = document.querySelector('.recipe');
-  #errorMessage = 'We could not find that recipe. Please try another one!';
-  #message = '';
-  #data;
-
-  render(data) {
-    this.#data = data;
-
-    this.#clear();
-    this.#parentElement.insertAdjacentHTML(
-      'afterbegin',
-      this.#generateMarkup()
-    );
-  }
-
-  renderSpinner() {
-    const markUp = `
-      <div class="spinner">
-        <svg>
-          <use href="${icons}#icon-loader"></use>
-        </svg>
-      </div>
-    `;
-
-    this.#parentElement.innerHTML = '';
-    this.#parentElement.insertAdjacentHTML('afterbegin', markUp);
-  }
-
-  renderError(message = this.#errorMessage) {
-    const markup = `<div class="error">
-      <div>
-        <svg>
-          <use href="${icons}#icon-alert-triangle"></use>
-        </svg>
-      </div>
-      <p>${message}</p>
-    </div>`;
-
-    this.#clear();
-    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
-  }
-
-  renderMessage(message = this.#message) {
-    const markup = `<div class="message">
-      <div>
-        <svg>
-          <use href="${icons}#icon-smile"></use>
-        </svg>
-      </div>
-      <p>${message}</p>
-    </div>`;
-
-    this.#clear();
-    this.#parentElement.insertAdjacentHTML('afterbegin', markup);
-  }
+class RecipeView extends View {
+  _parentElement = document.querySelector('.recipe');
 
   addHandlerRender(handler) {
     ['hashchange', 'load'].forEach(event =>
@@ -65,18 +12,14 @@ class RecipeView {
     );
   }
 
-  #clear() {
-    this.#parentElement.innerHTML = '';
-  }
-
-  #generateMarkup() {
+  _generateMarkup() {
     return `
       <figure class="recipe__fig">
-        <img src="${this.#data.image}" alt="${
-      this.#data.title
+        <img src="${this._data.image}" alt="${
+      this._data.title
     }" class="recipe__img" />
         <h1 class="recipe__title">
-          <span>${this.#data.title}</span>
+          <span>${this._data.title}</span>
         </h1>
       </figure>
 
@@ -86,7 +29,7 @@ class RecipeView {
             <use href="${icons}#icon-clock"></use>
           </svg>
           <span class="recipe__info-data recipe__info-data--minutes">${
-            this.#data.cookingTime
+            this._data.cookingTime
           }</span>
           <span class="recipe__info-text">minutes</span>
         </div>
@@ -95,7 +38,7 @@ class RecipeView {
             <use href="${icons}#icon-users"></use>
           </svg>
           <span class="recipe__info-data recipe__info-data--people">${
-            this.#data.servings
+            this._data.servings
           }</span>
           <span class="recipe__info-text">servings</span>
 
@@ -128,7 +71,7 @@ class RecipeView {
       <div class="recipe__ingredients">
         <h2 class="heading--2">Recipe ingredients</h2>
         <ul class="recipe__ingredient-list">
-          ${this.#data.ingredients
+          ${this._data.ingredients
             ?.map(this.#generateMarkupIngredients)
             .join('')}
         </ul>
@@ -138,12 +81,12 @@ class RecipeView {
         <h2 class="heading--2">How to cook it</h2>
         <p class="recipe__directions-text">
           This recipe was carefully designed and tested by
-          <span class="recipe__publisher">${this.#data.publisher}</span>. Please
+          <span class="recipe__publisher">${this._data.publisher}</span>. Please
           check out directions at their website.
         </p>
         <a
           class="btn--small recipe__btn"
-          href="${this.#data.sourceUrl}"
+          href="${this._data.sourceUrl}"
           target="_blank"
         >
           <span>Directions</span>
