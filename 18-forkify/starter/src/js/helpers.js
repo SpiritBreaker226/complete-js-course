adb7e2f959889a6d975503b1ed4cf09668054693
Jason Stathopulos <jstathopulos@gmail.com>
2022-12-06 16:22:55 -0800

    Add Promise Timeout For Slow Connections
    Add a Promise Timeout for slow connections so that if there is a slow
    connection, stop the promise from continuing and try again later or
    error out.

--- 18-forkify/starter/src/js/helpers.js
+++ 18-forkify/starter/src/js/helpers.js
@@ -1,5 +1,17 @@
+import { TIMEOUT_SECONDS } from './config';
+
+const timeout = function (seconds) {
+  return new Promise(function (_, reject) {
+    setTimeout(function () {
+      reject(
+        new Error(`Request took too long! Timeout after ${seconds} second(s)`)
+      );
+    }, seconds * 1000);
+  });
+};
+
 export const getJSON = async url => {
-  const res = await fetch(url);
+  const res = await Promise.race([fetch(url), timeout(TIMEOUT_SECONDS)]);
 
   const data = await res.json();
 
